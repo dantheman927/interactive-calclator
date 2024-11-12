a5f867196ff7948f909764ca7e9b31a8d090f2d0
smullu123 <137054934+dantheman927@users.noreply.github.com>
2024-11-12 20:53:14 +0000

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,45 +1,55 @@
 class calfunctions:
-    def add(x,y):
-        print(x+y)
+    def add(x, y):
+        print(x + y)
 
-    def sub(x,y):
-        print(x-y)
+    def sub(x, y):
+        print(x - y)
 
-    def tim(x,y): 
-        print(x*y)
+    def tim(x, y): 
+        print(x * y)
 
-    def div(x,y):
-        print(x/y)
+    def div(x, y):
+        print(x / y)
 
     def squ(x):
         print(x * x)
 
 cf = calfunctions
 
-def calculor(caltype):
-    
-    caltype = caltype.split(" ")
-    
-    while (caltype.count("")):
-        caltype.remove("") 
+while True:
+    def calculor(caltype):
+            try:
+                
+                
+                caltype = caltype.split(" ")
+                
+                
+                while (caltype.count("")):
+                    caltype.remove("") 
 
+                
+                cal0 = int(caltype[0])
+                if "^" != caltype[1]:  
+                    cal2 = int(caltype[2])
+            
+                
+                if caltype[1] in ["+", "add"]:
+                    cf.add(cal0, cal2)
+                elif caltype[1] in ["-", "sub"]:
+                    cf.sub(cal0, cal2)
+                elif caltype[1] in ["*", "times"]:
+                    cf.tim(cal0, cal2)
+                elif caltype[1] in ["/", "div"]:
+                    cf.div(cal0, cal2)
+                elif caltype[1] == "^":
+                    cf.squ(cal0)
 
-    cal0 = int(caltype[0])
-    if "^"  != caltype[1]:
-        cal2 = int(caltype[2])
-    
-    if caltype[1] in ["+","add"]:
-       cf.add(cal0,cal2)
-    elif caltype[1] in ["-","sub"]:
-        cf.sub(cal0,cal2)
-    elif caltype[1] in ["*","times"]:
-        cf.tim(cal0,cal2)
-    elif caltype[1] in ["/","div"]:
-       cf.div(cal0,cal2)
-    elif caltype[1] in ["^"]:        
-        cf.squ(cal0)
+            
+            except ValueError:
+                print("Please enter a valid calculation, like '5 + 5' or '4 ^' for square.")
 
-calinput = input("enter a what you want e.g 5 + 5\n")
+        
 
-calculor(calinput)
+    calinput = input("Enter what you want to calculate, e.g.,'5 + 5' or '4 ^' for square:\n")
+    calculor(calinput)
 
